fd10d6b05508b1f505227b29f609ebe5429c944f
SuperNoahPlushes <89299730+SuperNoahPlushes@users.noreply.github.com>
2023-10-28 12:41:48 -0600

    Fixed multiple small issues, including "Tita's Channel" button

---
+++ _vti_cnf/base.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|28 Oct 2023 18:16:19 -0000
+vti_author:SR|SNP-PC\\Super
+vti_modifiedby:SR|SNP-PC\\Super
+vti_nexttolasttimemodified:TW|28 Oct 2023 18:16:19 -0000
+vti_timecreated:TR|28 Oct 2023 18:40:30 -0000
+vti_cacheddtm:TX|28 Oct 2023 18:40:30 -0000
+vti_filesize:IR|2418512
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|videoseries.htm
